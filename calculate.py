942494aee3deec0925e38a5d953f974c0f5b1e83
nongtiny <tinyutb3612@gmail.com>
2018-11-03 23:53:06 +0700

    Noob hand detection

--- calculate.py
+++ calculate.py
@@ -1,19 +1,21 @@
 import cv2
-import os
 import numpy as np
 
-img=cv2.imread("result.png",0)
-ret,thresh1 = cv2.threshold(img,127,255,cv2.THRESH_BINARY)
-_, contours, hierarchy = cv2.findContours(thresh1, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-cnt = max(contours, key = lambda x: cv2.contourArea(x))
+def findShape(frame):
+    # ret,thresh1 = cv2.threshold(frame,127,255,cv2.THRESH_BINARY)
+    _, contours, _ = cv2.findContours(frame, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    return contours
 
-print(cnt)
 
-hull = cv2.convexHull(cnt)
+# img=cv2.imread("testResultRight.png",0)
+# _, contours, hierarchy = cv2.findContours(img, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+# print(contours[0])
 
-areaHull = cv2.contourArea(hull)
-print("Hull area ",areaHull)
-areaCnt = cv2.contourArea(cnt)
-print("Contour area", areaCnt)
-areaRatio = ((areaHull - areaCnt)/areaCnt) * 100 
-print("Ratio", areaRatio)
+# hull = cv2.convexHull(cnt)
+
+# areaHull = cv2.contourArea(hull)
+# print("Hull area ",areaHull)
+# areaCnt = cv2.contourArea(cnt)
+# print("Contour area", areaCnt)
+# areaRatio = ((areaHull - areaCnt)/areaCnt) * 100 
+# print("Ratio", areaRatio)
